347b765c4f837e3ee9967c942da20ed17c3259b0
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2020-08-09 23:19:05 +1000

    Add Progress and Summary components

---
+++ src/Progress.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let total = 100;
+  export let value = 0;
+  export let color = "var(--app-primary-color)";
+  export let size = "4px";
+  let percent = (value * 100) / total;
+</script>
+
+<style>
+  .total {
+    position: relative;
+  }
+
+  .value {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    background-image: repeating-linear-gradient(
+      45deg,
+      transparent,
+      transparent 4px,
+      rgba(255, 255, 255, 0.5) 4px,
+      rgba(255, 255, 255, 0.5) 8px
+    );
+  }
+</style>
+
+<div class="total" style="height:{size};">
+  <div class="value" style="width:{percent}%;background-color:{color}" />
+</div>
---
+++ src/Summary.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Progress from "./Progress.svelte";
+
+  export let acceptedContributions = 41;
+  export let pendingContributions = 23;
+  export let reviewed = 72;
+  export let total = 100;
+  export let size = "4px";
+</script>
+
+<style>
+  .summary {
+    position: relative;
+    width: 100%;
+  }
+
+  .metric {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+  }
+</style>
+
+<div class="summary" style="height:{size}">
+  <div class="metric">
+    <Progress {total} {size} value={total} color="var(--muted-color)" />
+  </div>
+  <div class="metric">
+    <Progress {total} {size} value={reviewed} color="dodgerblue" />
+  </div>
+  <div class="metric">
+    <Progress
+      {total}
+      {size}
+      value={acceptedContributions + pendingContributions}
+      color="gold" />
+  </div>
+  <div class="metric">
+    <Progress {total} {size} value={acceptedContributions} color="limegreen" />
+  </div>
+</div>
