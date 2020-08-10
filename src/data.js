a7af17bf0af19f525813eef7f1d2e6e86bd0346a
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2020-08-10 21:20:26 +1000

    Refactor extract example data for convenience

---
+++ src/data.js
@@ -0,0 +1,132 @@
+export const lists = [
+  {
+    name: "3 letras",
+    description:
+      "Sustantivos de tres letras, por frequencia de uso (fuente: CORPES XXI).",
+    guidelines: {
+      title: "Guidelines (Spanish)",
+      url: "https://memorablewords.org",
+    },
+    words: [
+      {
+        value: "ajo",
+        reviews: [
+          {
+            reviewer: "gonzalo-bulnes",
+            decision: "yes",
+            reviewedAt: "2020-08-02T09:45:00",
+          },
+        ],
+      },
+      {
+        value: "pez",
+        reviews: [
+          {
+            reviewer: "gonzalo-bulnes",
+            decision: "no",
+            reviewedAt: "2020-08-02T09:45:00",
+          },
+        ],
+      },
+      {
+        value: "sol",
+        reviews: [
+          {
+            reviewer: "amanda",
+            decision: "no",
+            reviewedAt: "2020-08-02T09:45:00",
+          },
+        ],
+      },
+      ,
+      {
+        value: "haz",
+        reviews: [],
+      },
+    ],
+  },
+  {
+    name: "4 letras",
+    description:
+      "Sustantivos de cuatro letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [
+      {
+        value: "tajo",
+        reviews: [
+          {
+            reviewer: "gonzalo-bulnes",
+            decision: "yes",
+            reviewedAt: "2020-08-02T09:45:00",
+          },
+        ],
+      },
+      {
+        value: "puro",
+        reviews: [
+          {
+            reviewer: "gonzalo-bulnes",
+            decision: "no",
+            reviewedAt: "2020-08-02T09:45:00",
+          },
+        ],
+      },
+      {
+        value: "jugo",
+        reviews: [
+          {
+            reviewer: "amanda",
+            decision: "no",
+            reviewedAt: "2020-08-02T09:45:00",
+          },
+        ],
+      },
+      ,
+      {
+        value: "zumo",
+        reviews: [],
+      },
+    ],
+  },
+  {
+    name: "5 letras",
+    description:
+      "Sustantivos de cinco letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+  {
+    name: "6 letras",
+    description:
+      "Sustantivos de seis letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+  {
+    name: "7 letras",
+    description:
+      "Sustantivos de siete letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+  {
+    name: "8 letras",
+    description:
+      "Sustantivos de ocho letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+  {
+    name: "9 letras",
+    description:
+      "Sustantivos de nueve letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+  {
+    name: "10 letras",
+    description:
+      "Sustantivos de diez letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+  {
+    name: "11 letras",
+    description:
+      "Sustantivos de once letras, por frequencia de uso (fuente: CORPES XXI).",
+    words: [],
+  },
+];
