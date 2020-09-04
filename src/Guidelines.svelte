7391eaac5d59bdc91d981afbcdd29f89d9c2d37d
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2020-09-04 17:57:58 +1000

    Add placeholder Guidelines page

---
+++ src/Guidelines.svelte
@@ -0,0 +1,56 @@
+<script>
+  import { _ } from "svelte-i18n";
+  import { Text } from "memorablewords-svelte-components";
+</script>
+
+<style>
+  section {
+    box-sizing: border-box;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100%;
+    padding: var(--app-padding);
+  }
+
+  header {
+    align-items: center;
+    display: flex;
+    justify-content: start;
+    padding: 0 1.5rem;
+  }
+
+  main {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-evenly;
+    padding: 0 calc(3 * var(--app-padding));
+  }
+
+  footer {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: var(--app-touch-target-size);
+  }
+  footer small {
+    font-style: oblique;
+  }
+</style>
+
+<section>
+  <header />
+  <main>
+    <Text element="p">{$_('guidelines_placeholder')}</Text>
+  </main>
+
+  <footer>
+    <Text>
+      <small>
+        {@html $_('purpose_reminder')}
+      </small>
+    </Text>
+  </footer>
+</section>
