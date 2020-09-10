d0a96a1e5cfbaa7a020e13f8ada888d5e90852d8
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2020-09-10 19:03:09 +1000

    Add Progress component

---
+++ src/internal/Progress.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { Text } from "memorablewords-svelte-components";
+
+  export let total = 0;
+  export let pending = 0;
+
+  $: progress = (total * 100) / pending;
+</script>
+
+<style>
+  .container {
+    display: flex;
+    width: 100%;
+  }
+
+  progress {
+    flex: auto;
+    margin-right: var(--app-padding);
+    width: 100%;
+  }
+
+  .count {
+    flex: content;
+    margin-left: var(--app-padding);
+    text-align: right;
+    white-space: nowrap;
+  }
+</style>
+
+<div class="container">
+  <progress max={total} value={total - pending} />
+
+  <div class="count">
+    <Text>
+      <small>{total - pending} / {total}</small>
+    </Text>
+  </div>
+</div>
